983031a13b5137f818c1fa94f426c86e7248e29e
JhonVaron <javy_37@hotmail.com>
2021-10-21 20:11:21 -0500

    proyecto completo

---
+++ app/views/personaDetailView.py
@@ -0,0 +1,14 @@
+from rest_framework import generics, status
+from rest_framework.response import Response
+
+from app.models.persona import Persona
+from app.serializers.personaSerializer import PersonaSerializer
+
+
+class PersonaDetailView(generics.RetrieveAPIView):
+    queryset = Persona.objects.all()
+    serializer_class = PersonaSerializer
+
+    def get(self, request, *args, **kwargs):
+
+        return super().get(request, *args, **kwargs)
